b452c5e0ecf3eb673394e0b3d2f84a665708df8f
Teelirium <angryzombi01@gmail.com>
2022-11-15 19:12:44 +0500

    add registration for all roles

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { hash } from "bcryptjs";
 import type { NextApiHandler } from "next";
-import { UserFormData } from "../../../types/userData";
+import { UserFormData, UserData } from "../../../types/userData";
 
 const prisma = new PrismaClient();
 
@@ -9,23 +9,38 @@ const handler: NextApiHandler = async (req, res) => {
   switch (req.method) {
     case "POST":
       const { user } = req.body as { user: UserFormData };
-      console.log('Registering user: ', user);
+      console.log("Registering user: ", user);
       const password = await hash(user.password, 12);
+
+      const data: UserData = {
+        name: user.name,
+        surname: user.surname,
+        middleName: user.middleName,
+        username: user.username,
+        password,
+      }
 
       switch (user.role) {
         case "PARENT":
-            await prisma.parent.create({
-                data: {
-                    name: user.name,
-                    surname: user.surname,
-                    middleName: user.middleName,
-                    username: user.username,
-                    password
-                }
-            })
-            break;
+          await prisma.parent.create({
+            data
+          });
+          break;
+        case "TEACHER":
+          await prisma.teacher.create({
+            data
+          })
+          break;
+        case 'WORKER':
+        case 'ADMIN':
+          await prisma.worker.create({
+            data: {
+              ...data,
+              role: user.role,
+            }
+          })
         default:
-            return res.status(501).send('Not implemented');
+          return res.status(422).send("Invalid role");
       }
       return res.send("OK");
 
