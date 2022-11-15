f618f6dbe8450f250b4da166bd702af4eec0bb68
Teelirium <angryzombi01@gmail.com>
2022-11-15 16:57:47 +0500

    add dish schema

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -6,11 +6,11 @@ const prisma = new PrismaClient();
 const handler: NextApiHandler = async (req, res) => {
   const data = await prisma.student.findMany({
     where: {
-        parentStudent: {
-            every: {
-                parentId: 5
-            }
-        }
+      parentStudent: {
+        every: {
+          parentId: 5,
+        },
+      },
     },
   });
   return res.json(data);
