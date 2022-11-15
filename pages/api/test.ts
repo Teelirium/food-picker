28a05c0015091acc6f5110c7b8c67b646241fee6
Teelirium <angryzombi01@gmail.com>
2022-11-15 16:00:25 +0500

    add student to parent relation

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,11 +1,19 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 import type { NextApiHandler } from "next";
 
 const prisma = new PrismaClient();
 
 const handler: NextApiHandler = async (req, res) => {
-    const data = await prisma.parent.findMany();
-    return res.json(data);
-}
+  const data = await prisma.student.findMany({
+    where: {
+        parentStudent: {
+            every: {
+                parentId: 5
+            }
+        }
+    },
+  });
+  return res.json(data);
+};
 
 export default handler;
