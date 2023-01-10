06fda640caf0a5e6258bf9fa7062a7ad2d19696e
Teelirium <angryzombi01@gmail.com>
2023-01-10 19:54:08 +0500

    algorithm optimization

--- pages/api/grades/total-orders.ts
+++ pages/api/grades/total-orders.ts
@@ -1,4 +1,4 @@
-import { Dish, Grade, Prisma } from "@prisma/client";
+import { Dish, DishType, Grade, Prisma } from "@prisma/client";
 import { NextApiHandler } from "next";
 import { getServerSideSession } from "utils/getServerSession";
 import prisma from "utils/prismaClient";
@@ -10,9 +10,7 @@ const paramSchema = z.object({
   day: dayOfWeekSchema.optional(),
 });
 
-export type GradeInfo = Grade & {
-  dishes: DishWithOrders[];
-};
+export type GradeInfo = Awaited<ReturnType<typeof getHandler>>;
 
 type DishWithOrders = Prisma.DishGetPayload<{
   select: { id: true; name: true; weightGrams: true; type: true };
@@ -38,83 +36,7 @@ const handler: NextApiHandler = async (req, res) => {
   switch (req.method) {
     case "GET": {
       const { day } = paramSchema.parse(req.query);
-
-      const defaultPrefs = await prisma.preference.findMany({
-        where: {
-          isDefault: true,
-        },
-        select: {
-          Dish: {
-            select: {
-              id: true,
-              name: true,
-              weightGrams: true,
-              type: true,
-            },
-          },
-        },
-      });
-
-      const grades = await prisma.grade.findMany({});
-      const result: GradeInfo[] = [];
-      for (let grade of grades) {
-        const studentCount = await prisma.student.count({
-          where: {
-            gradeId: grade.id,
-          },
-        });
-
-        const defaultDishes = defaultPrefs
-          .map((pref) => {
-            if (pref.Dish !== null)
-              return {
-                ...pref.Dish,
-                _count: { preferences: studentCount },
-              };
-          })
-          .filter((dish): dish is DishWithOrders => dish !== undefined);
-
-        const dishes = await prisma.dish.findMany({
-          select: {
-            id: true,
-            name: true,
-            weightGrams: true,
-            type: true,
-            _count: {
-              select: {
-                preferences: {
-                  where: {
-                    dayOfWeek: day,
-                    Student: {
-                      gradeId: grade.id,
-                    },
-                  },
-                },
-              },
-            },
-          },
-        });
-        const nonEmpty = dishes.filter((dish) => dish._count.preferences > 0);
-
-        for (let dish of nonEmpty) {
-          const index = defaultDishes.findIndex((d) => d.type === dish.type); //inefficient
-
-          if (index >= 0 && defaultDishes[index].id !== dish.id) {
-            defaultDishes[index]._count.preferences -= dish._count.preferences;
-          }
-        }
-
-        const gradeWithDishes = {
-          ...grade,
-          dishes: [
-            ...nonEmpty,
-            ...defaultDishes.filter((dish) => dish._count.preferences > 0),
-          ],
-        };
-        result.push(gradeWithDishes);
-      }
-
-      return res.json(result);
+      return res.json(await getHandler(day));
     }
     default: {
       return res.status(405).send("");
@@ -123,3 +45,90 @@ const handler: NextApiHandler = async (req, res) => {
 };
 
 export default handler;
+
+async function getHandler(day?: number) {
+  const defaultPrefs = await prisma.preference.findMany({
+    where: {
+      isDefault: true,
+    },
+    select: {
+      Dish: {
+        select: {
+          id: true,
+          name: true,
+          weightGrams: true,
+          type: true,
+        },
+      },
+    },
+  });
+
+  const grades = await prisma.grade.findMany({});
+  const result = [];
+  for (let grade of grades) {
+    const studentCount = await prisma.student.count({
+      where: {
+        gradeId: grade.id,
+      },
+    });
+
+    const defaultDishMap = new Map(
+      defaultPrefs
+      .map((pref) => {
+        if (pref.Dish !== null)
+        return [
+          pref.Dish.type,
+          {
+            ...pref.Dish,
+            _count: { preferences: studentCount },
+          },
+        ];
+      })
+      .filter((el): el is [DishType, DishWithOrders] => el !== undefined)
+    );
+
+    const dishes = await prisma.dish.findMany({
+      select: {
+        id: true,
+        name: true,
+        weightGrams: true,
+        type: true,
+        _count: {
+          select: {
+            preferences: {
+              where: {
+                dayOfWeek: day,
+                Student: {
+                  gradeId: grade.id,
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+    const nonEmpty = dishes.filter((dish) => dish._count.preferences > 0);
+    
+    for (let dish of nonEmpty) {
+      const existing = defaultDishMap.get(dish.type);
+
+      if (!!existing && existing.id !== dish.id) {
+        existing._count.preferences -= dish._count.preferences;
+
+        if (existing._count.preferences > 0) {
+          defaultDishMap.set(dish.type, existing);
+        } else {
+          defaultDishMap.delete(dish.type);
+        }
+      }
+    }
+
+    const gradeWithDishes = {
+      ...grade,
+      dishes: [...nonEmpty, ...Array.from(defaultDishMap.values())],
+    };
+    result.push(gradeWithDishes);
+  }
+
+  return result;
+}
