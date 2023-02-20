da84298c8446e81723412327f60eecfd302e1023
Teelirium <angryzombi01@gmail.com>
2023-02-20 16:14:32 +0500

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,9 @@ import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
 
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
-
   return (
     <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     </SessionProvider>
   );
 }
