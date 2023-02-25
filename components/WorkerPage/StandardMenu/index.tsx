aa8ee1ac779fae76d928d5e6c085b31804524afe
Teelirium <angryzombi01@gmail.com>
2023-02-25 19:51:51 +0500

    fetch and display of default prefs

--- components/WorkerPage/StandardMenu/index.tsx
+++ components/WorkerPage/StandardMenu/index.tsx
@@ -1,8 +1,11 @@
+import axios from 'axios';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useQuery } from 'react-query';
 import { z } from 'zod';
 
 import PreferenceSection from 'components/PreferenceSection';
+import { DefaultDishes } from 'pages/api/preferences/default';
 import { DishType } from 'types/Dish';
 import dayMap from 'utils/dayMap';
 import dishTypeMap from 'utils/dishTypeMap';
@@ -17,10 +20,19 @@ const paramSchema = z.object({
 
 const dishTypes: DishType[] = ['PRIMARY', 'SECONDARY', 'SIDE', 'DRINK'];
 
+async function getDishes(day: number) {
+  const data = (await axios
+    .get(`/api/preferences/default?day=${day}`)
+    .then((res) => res.data)) as DefaultDishes;
+  // console.log('fetched', day);
+  return new Map(data.map((d) => [d.Dish.type, d.Dish]));
+}
+
 const StandardMenu: React.FC = () => {
   const router = useRouter();
   const { day } = paramSchema.parse(router.query);
 
+  const query = useQuery(['defaults', day], () => getDishes(day), { keepPreviousData: true });
   return (
     <div className={styles.container}>
       <header className={styles.header}>
@@ -34,9 +46,11 @@ const StandardMenu: React.FC = () => {
         {/* <span>Видеоинструкция</span> */}
       </header>
       <main className={styles.body}>
-        {dishTypes.map((type) => (
-          <PreferenceSection key={type} title={dishTypeMap[type]} />
-        ))}
+        {(query.isLoading || query.isPreviousData) && <span>Загрузка...</span>}
+        {query.isSuccess &&
+          dishTypes.map((type) => (
+            <PreferenceSection key={type} title={dishTypeMap[type]} dish={query.data.get(type)} />
+          ))}
       </main>
       <footer className={styles.footer}>(Меню повторяется каждую неделю)</footer>
     </div>
