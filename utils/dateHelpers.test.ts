0232b8912736f6132a00ac40ae80368c0425d6ac
Teelirium <angryzombi01@gmail.com>
2023-04-08 19:32:55 +0500

    add some more tests

--- utils/dateHelpers.test.ts
+++ utils/dateHelpers.test.ts
@@ -1,15 +1,25 @@
 import { addDaysToDate, getNextMonday, stripTimeFromDate } from './dateHelpers';
 
 describe('getNextMonday()', () => {
-  it('should return date of the next monday', () => {
+  it('returns date of the next monday', () => {
     const initial = new Date('2023-03-21');
     const result = getNextMonday(initial);
     expect(result.toISOString()).toEqual(new Date('2023-03-27').toISOString());
   });
+  it('works when given a Sunday', () => {
+    const initial = new Date('2023-03-26');
+    const result = getNextMonday(initial);
+    expect(result.toISOString()).toEqual(new Date('2023-03-27').toISOString());
+  });
+  it('works when crossing months', () => {
+    const initial = new Date('2023-03-30');
+    const result = getNextMonday(initial);
+    expect(result.toISOString()).toEqual(new Date('2023-04-03').toISOString());
+  });
 });
 
 describe('addDaysToDate()', () => {
-  it('should add days correctly', () => {
+  it('adds days correctly', () => {
     const initial = new Date('2023-04-01');
     const result = addDaysToDate(initial, 4);
     expect(result.toISOString()).toEqual(new Date('2023-04-05').toISOString());
@@ -17,7 +27,7 @@ describe('addDaysToDate()', () => {
 });
 
 describe('stripTimeFromDate()', () => {
-  it('should strip time correctly', () => {
+  it('strips time correctly', () => {
     const initial = new Date('2023-04-01T08:08:08');
     const result = stripTimeFromDate(initial);
     expect(result.toISOString()).toEqual(new Date('2023-04-01').toISOString());
