5b1aebed1f878cfb0b219a4b6843740a1ca9ee6b
Teelirium <angryzombi01@gmail.com>
2023-06-10 13:32:50 +0500

    ignoring test file

--- pages/api/test.ts
+++
@@ -1,7 +0,0 @@
-import { TRPCError } from '@trpc/server';
-
-import withErrHandler from 'utils/errorUtils/withErrHandler';
-
-export default withErrHandler(async (req, res) => {
-  if (process.env.NODE_ENV === 'production') throw new TRPCError({ code: 'FORBIDDEN' });
-});
