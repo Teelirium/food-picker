ef2f004b6c8deca7a7b963905fa061fa1f619ea0
Teelirium <angryzombi01@gmail.com>
2023-06-14 19:57:52 +0500

    full csv parsing

--- pages/api/workers/csv.ts
+++ pages/api/workers/csv.ts
@@ -1,10 +1,19 @@
 import { createReadStream } from 'fs';
+import * as readline from 'node:readline';
 
 import { TRPCError } from '@trpc/server';
+import DetectFileEncodingAndLanguage from 'detect-file-encoding-and-language';
 import formidable from 'formidable';
+import { NextApiRequest } from 'next';
+import * as Papa from 'papaparse';
+import { transliterate } from 'transliteration';
 
+import { FullName } from 'modules/user/types';
+import { WorkerCreateForm } from 'modules/worker/types';
 import withErrHandler from 'utils/errorUtils/withErrHandler';
 import { getServerSessionWithOpts } from 'utils/getServerSession';
+import { getInitials } from 'utils/names';
+import { getRandomPassword } from 'utils/randomUtils';
 import verifyRole from 'utils/verifyRole';
 
 export const config = {
@@ -12,6 +21,34 @@ export const config = {
     bodyParser: false,
   },
 };
+
+function parseFiles(req: NextApiRequest) {
+  const form = new formidable.IncomingForm({ keepExtensions: true });
+  return new Promise<formidable.Files>((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) reject(err);
+      resolve(files);
+    });
+  });
+}
+
+function readCsvFile(filepath: string) {
+  return new Promise<string[]>((resolve, reject) => {
+    const arr: string[] = [];
+    try {
+      const outStream = createReadStream(filepath, { encoding: 'utf-8' });
+      const rl = readline.createInterface({ input: outStream, crlfDelay: Infinity });
+      rl.on('line', (data) => {
+        arr.push(data);
+      });
+      rl.on('close', () => {
+        resolve(arr);
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
 
 export default withErrHandler(async (req, res) => {
   const session = await getServerSessionWithOpts({ req, res });
@@ -23,23 +60,51 @@ export default withErrHandler(async (req, res) => {
   }
   switch (req.method) {
     case 'POST': {
-      const form = new formidable.IncomingForm({ keepExtensions: true });
+      const files = await parseFiles(req);
 
-      const files = await new Promise<formidable.Files>((resolve, reject) => {
-        form.parse(req, (err, fields, files) => {
-          if (err) reject(err);
-          resolve(files);
+      const csvFile = files.csv as formidable.File | undefined;
+      if (!csvFile) {
+        throw new TRPCError({ code: 'BAD_REQUEST', message: 'Не найден файл в поле `csv`' });
+      }
+      console.log(csvFile.filepath);
+
+      const encoding = await DetectFileEncodingAndLanguage(csvFile.filepath);
+      if (encoding.encoding !== 'UTF-8') {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'Кодировка файла должна быть `utf-8`',
         });
-      });
-      const { csv } = files as { csv: formidable.File };
+      }
 
-      const outStream = createReadStream(csv.filepath, { encoding: 'utf-8' });
-      outStream.on('data', (data) => {
-        console.log(data);
-        res.send(data);
-        outStream.close();
+      const data = await readCsvFile(csvFile.filepath);
+      const parsed = data
+        .slice(1)
+        .map((line) => Papa.parse<string[]>(line).data)
+        .flat();
+
+      const workers = parsed.map((el) => {
+        const fullName = {
+          surname: el[0],
+          name: el[1],
+          middleName: el[2] || null,
+        } satisfies FullName;
+        const initals = getInitials(fullName).replaceAll(' ', '');
+        const nums = 4;
+        const randDiscriminator = Math.floor(
+          Math.random() * (10 ** nums - 10 ** (nums - 1)) + 10 ** (nums - 1),
+        );
+        const randUsername = `${transliterate(initals)}-${randDiscriminator}`;
+        const randPassword = getRandomPassword(10);
+        const dto = {
+          ...fullName,
+          username: randUsername,
+          password: randPassword,
+          role: el[3] ? 'ADMIN' : 'WORKER',
+        } satisfies WorkerCreateForm;
+        return dto;
       });
-      return;
+
+      return res.json(workers);
     }
     default:
       throw new TRPCError({ code: 'METHOD_NOT_SUPPORTED' });
---
+++ utils/randomUtils/index.ts
@@ -0,0 +1,13 @@
+const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '0123456789';
+// const symbols = '!@#$%^&*_-+=';
+
+const chars = alpha + numbers;
+
+export function getRandomPassword(length: number) {
+  let password = '';
+  for (let i = 0; i < length; i += 1) {
+    password += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return password;
+}
